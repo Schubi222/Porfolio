ec7f4bc6587e980a3cc1c243748290ce785274ce
Schubi222 <73842560+Schubi222@users.noreply.github.com>
2023-07-01 19:38:36 +0200

    HomeView basics

--- Porfolio/src/views/HomeView.vue
+++ Porfolio/src/views/HomeView.vue
@@ -1,13 +1,17 @@
 <template>
-  <div id="HomeView">
+  <div class="Home-View">
     <h1 class="test">HOME</h1>
+    <PerfectTextField :text="text" class="Home-View__text-field"/>
+
+
   </div>
 </template>
 
 <script setup>
-
+  import PerfectTextField from "@/components/PerfectTextField.vue";
+  const text = "There is something you could do, you would do, that would make a difference.\n\n ~Jordan B. Peterson"
 </script>
 
 <style scoped>
-
+  @import "@/assets/HomeView.css";
 </style>
