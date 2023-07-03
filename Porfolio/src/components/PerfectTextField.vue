03f8114112069c7cf6e0cde0c50155df65cc4d66
Schubi222 <73842560+Schubi222@users.noreply.github.com>
2023-07-03 10:27:48 +0200

    Button style | possibility for bottom link added to TextField

--- Porfolio/src/components/PerfectTextField.vue
+++ Porfolio/src/components/PerfectTextField.vue
@@ -2,13 +2,16 @@
   <div class="Text-Field-Wrapper">
     <div class="Text-Field-Wrapper__heading" v-if="props.heading">{{props.heading}}</div>
     <div class="Text-Field-Wrapper__text">{{props.text}}</div>
+    <RouterLink v-if="props.link" :to="'/'+props.link.to" class="Text-Field-Wrapper__text Text-Field-Wrapper__link">
+      {{props.link.text}}
+    </RouterLink>
   </div>
 
 </template>
 
 <script setup>
-  const props = defineProps(['heading','text'])
-
+  import {RouterLink} from "vue-router";
+  const props = defineProps(['heading','text', 'link'])
 
 </script>
 
