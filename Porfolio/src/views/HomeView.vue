c37fb6e6302f7a4355383946c4933e061398ead1
Schubi222 <73842560+Schubi222@users.noreply.github.com>
2023-07-03 10:28:56 +0200

    HomeView Changes: Button added | about link added | moved some style from vue into css

--- Porfolio/src/views/HomeView.vue
+++ Porfolio/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="Home-View">
     <h1 class="test">HOME</h1>
-    <PerfectTextField :text="text" class="Home-View__text-field"/>
+    <PerfectTextField :text="about_text" :link="about_link" class="Home-View__text-field"/>
     <swiper
         :centered-slides="true"
         :slides-per-view="4"
@@ -18,6 +18,7 @@
       <swiper-slide class="swiper_custom_slide">Slide 3</swiper-slide>
       <swiper-slide class="swiper_custom_slide">Slide 4</swiper-slide>
     </swiper>
+   <RouterLink class="btn big_btn" to="/projects">All Projects</RouterLink>
 
   </div>
 </template>
@@ -26,38 +27,25 @@
   import PerfectTextField from "@/components/PerfectTextField.vue";
   // Import Swiper Vue.js components
   import { Swiper, SwiperSlide } from 'swiper/vue';
-
   import {Scrollbar, Parallax} from "swiper";
 
   // Import Swiper styles
   import 'swiper/css';
   import 'swiper/css/scrollbar';
-  const text = "There is something you could do, you would do, that would make a difference.\n\n ~Jordan B. Peterson"
+
+  const about_text = "There is something you could do, you would do, that would make a difference.\n\n ~Jordan B. Peterson"
+  const about_link = {to: 'about', text:'Want to read more about me?'}
   const modules = [Scrollbar, Parallax]
 
 </script>
 
 <style scoped>
   @import "@/assets/HomeView.css";
-  .mySwiper{
-    margin: 0 20rem;
-    width: 80%;
-    height: 410px;
-    padding: 5rem 20rem;
-    overflow: hidden;
-  }
-  .swiper_custom_slide{
-    border: 1px solid white;
-    height: 400px;
-    overflow: hidden;
-    border-radius: 10px;
-    padding: 10px;
-    box-sizing: border-box;
-  }
+  @import "@/assets/SwiperCustomStyles.css";
 
-  .Home-View{
+/*  .Home-View{
     height: fit-content;
     width: 100%;
     align-self: center;
-  }
+  }*/
 </style>
