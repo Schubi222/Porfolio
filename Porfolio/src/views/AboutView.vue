e59fa91b5ff87d2e2007f82b3d6a4e2e924420dc
Schubi222 <73842560+Schubi222@users.noreply.github.com>
2023-07-06 08:28:30 +0200

    working on about

--- Porfolio/src/views/AboutView.vue
+++ Porfolio/src/views/AboutView.vue
@@ -1,7 +1,57 @@
 <template>
   <div class="About-View-Wrapper">
-    <div class="About-View-Wrapper__top-text"></div>
-    <div class="CV-Wrapper"></div>
+    <div class="About-View-Wrapper__top-text">
+      <h2 class="About-View-Wrapper__top-heading">Fabian Schubert</h2>
+      <h3 class="About-View-Wrapper__title">Web Developer</h3>
+      <div class="About-View-Wrapper__top-flow-text">
+        I build applications and websites striving for constant improvement.
+      </div>
+    </div>
+    <div class="CV-Wrapper">
+      <div class="CV-Wrapper__personal">
+        <p>
+          PLACEHOLDER:
+          This should be some text about myself concerning coding. Maybe from my beginnings to how I ended up in
+          Web dev and how I enjoy it.
+        </p>
+        <p>
+          Currently my main focus is gathering more experience with different web technologies
+          by working on different projects
+        </p>
+        <p>
+          When I am not immersed in coding I like to spend my time
+          <span id="Ice-Hockey-Animation">playing ice hockey</span>, <span id="Gym-Animation">working out</span>,
+          <span id="Read-Animation">reading books</span> or <span id="Socialise-Animation">socialising</span>.
+        </p>
+      </div>
+      <div class="CV-Wrapper__projects"></div>
+      <div class="CV-Wrapper__education">
+        <div class="Experience-Entry-Wrapper">
+          <div class="Experience-Entry-Wrapper__side-header">1998-PRESENT</div>
+          <div class="Experience-Entry-Wrapper__body">
+            <div class="Experience-Entry-Wrapper__body-heading">Living</div>
+            <div class="Experience-Entry-Wrapper__body-subheading">Human being</div>
+            <div class="Experience-Entry-Wrapper__body-text">
+              Improving oneself with the intention to create a better world for as many people as possible while living
+              fulfilled and also still having some fun.
+            </div>
+            <div class="Experience-Entry-Wrapper__body-tags">
+              <div class="Experience-Entry-Wrapper__body-tags-tag">Eating</div>
+              <div class="Experience-Entry-Wrapper__body-tags-tag">Drinking</div>
+              <div class="Experience-Entry-Wrapper__body-tags-tag">Public speaking</div>
+              <div class="Experience-Entry-Wrapper__body-tags-tag">NHL</div>
+              <div class="Experience-Entry-Wrapper__body-tags-tag">Ice Hockey</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="CV-Wrapper__jobs">
+        <p>
+
+        </p>
+        <a href="./src/assets/pdfs/Fabian_Schubert_Lebenslauf_eng.pdf" target="_blank">View full CV</a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,5 +61,5 @@
 
 
 <style scoped>
-
+  @import "@/assets/about.css";
 </style>
